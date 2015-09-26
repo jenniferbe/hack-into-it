33067b2f3eb4d26942b0762c9821cbc5a828a768
Quinn Tran <ssquinntran@berkeley.edu>
2015-09-26 11:17:08 -0700

    can query. need to test

---
+++ python/query_data.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sqlite3 as lite
+import sys
+
+con = lite.connect('people.db')
+
+def query(marriage_status, employment_status):
+    with con:   
+        cur = con.cursor()    
+        cur.execute("SELECT * FROM people WHERE marriage_status = " + marriage_status +
+                    " AND employment_status = " + employment_status)
+        rows = cur.fetchall()
+        for row in rows:
+        	print row
+
+
+con.close()
